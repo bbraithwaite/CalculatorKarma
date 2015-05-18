9f5a6bba280bf0764f38308c683c24cbba1c01ca
bbraithwaite <bradley.braithwaite@gmail.com>
2015-05-18 14:10:44 +0200

    add example

---
+++ app/calculator.controller.js
@@ -0,0 +1,7 @@
+angular.module('calculatorApp', []).controller('CalculatorController', function CalculatorController($scope) {
+  $scope.z = 0;
+  $scope.sum = function() {
+    $scope.z = $scope.x + $scope.y;
+  };
+});
+
